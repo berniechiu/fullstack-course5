0c60d156ad33be8cdf3c320d51d393d4d72fa857
Bernie Chiu <bernie_chiu@hotmail.com>
2017-08-18 07:59:19 +0800

    Search service

--- assignments/assignment3/module3-solution/app.js
+++ assignments/assignment3/module3-solution/app.js
@@ -1,23 +1,44 @@
 (function() {
   'use strict';
 
-  MenuSearchService.$inject = ['$http'];
-  function MenuSearchService($http) {
+  MenuSearchService.$inject = ['$http', '$filter', '$q'];
+  function MenuSearchService($http, $filter, $q) {
     var self = this;
-    var API_ENDPOINT = 'https://davids-restaurant.herokuapp.com'
+    var URL = {
+      GET: 'https://davids-restaurant.herokuapp.com'
+    };
+    var search = '';
+    var allItems = [];
+    var foundItems = [];
 
-    self.getMatchedMenuItems = function() {
-      return $http({ method: 'GET', url: (API_ENDPOINT + '/menu_items.json') }).then(function (res) {
-        return res.data.menu_items;
-      });
+    self.getMatchedMenuItems = function(searchTerm) {
+      search = searchTerm;
+      var promise =  (allItems.length > 0) ? $q.when(allItems) : $http({ method: 'GET', url: (URL.GET + '/menu_items.json') }).then(cacheItems)
+      return promise.then(filterItems);
+    }
+
+    function cacheItems(res) {
+      allItems = res.data.menu_items;
+      return allItems
+    }
+
+    function filterItems(items) {
+      foundItems = $filter('filter')(items, { name: search });
+      return foundItems;
     }
   }
 
   NarrowItDownController.$inject = ['MenuSearchService'];
   function NarrowItDownController(MenuSearchService) {
-    MenuSearchService.getMatchedMenuItems().then(function(items) {
-      // Do something
-    });
+    var self = this;
+    self.data = {
+      searchTerm: ''
+    };
+    self.searchMenu = function(searchTerm) {
+      MenuSearchService.getMatchedMenuItems(searchTerm).then(function(items) {
+        console.log(items);
+      });
+    };
   }
 
   angular
